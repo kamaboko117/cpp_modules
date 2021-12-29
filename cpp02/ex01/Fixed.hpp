4a52fe5f3bcb853b6001df112a1dbe983d4d70b2
Alexander SABOURET <asaboure@e2r7p1.clusters.42paris.fr>
2021-12-29 19:55:52 +0100

    ex01 done

--- cpp02/ex01/Fixed.hpp
+++ cpp02/ex01/Fixed.hpp
@@ -6,12 +6,13 @@
 /*   By: asaboure <asaboure@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/29 16:18:31 by asaboure          #+#    #+#             */
-/*   Updated: 2021/12/29 16:59:58 by asaboure         ###   ########.fr       */
+/*   Updated: 2021/12/29 18:36:10 by asaboure         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_HPP
 # define FIXED_HPP
+#include <ostream>
 
 class Fixed
 {
@@ -22,12 +23,19 @@ private:
 public:
 	Fixed();														// Canonical
 	Fixed(Fixed const &src);										// Canonical
+	Fixed(const int nb);
+	Fixed(const float nb);
 	~Fixed();														// Canonical
 
 	Fixed	&operator=(Fixed const &rhs);							// Canonical
 
+	float	toFloat(void) const;
+	int		toInt(void) const;
+
 	int		getRawBits(void) const;
 	void	setRawBits(int const raw);
 };
+
+std::ostream	&operator<<(std::ostream &o, Fixed const &i);
 
 #endif
--- cpp02/ex01/main.cpp
+++ cpp02/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: asaboure <asaboure@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/29 16:31:03 by asaboure          #+#    #+#             */
-/*   Updated: 2021/12/29 17:01:33 by asaboure         ###   ########.fr       */
+/*   Updated: 2021/12/29 19:44:32 by asaboure         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,19 @@
 int main(void) 
 {
 	Fixed a;
-	Fixed b(a);
-	Fixed c;
+	Fixed const b(10);
+	Fixed const c(42.42f);
+	Fixed const d(b);
 
-	c = b;
+	a = Fixed(1234.4321f);
+
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
 	
-	std::cout << a.getRawBits() << std::endl;
-	std::cout << b.getRawBits() << std::endl;
-	std::cout << c.getRawBits() << std::endl;
-	return 0;
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
 }
