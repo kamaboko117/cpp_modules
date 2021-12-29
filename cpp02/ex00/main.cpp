c944b658c7e2fe2ff061294c4cd28526566f381c
Alexander SABOURET <asaboure@e2r7p1.clusters.42paris.fr>
2021-12-29 17:03:34 +0100

    ex00 done

---
+++ cpp02/ex00/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asaboure <asaboure@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/29 16:31:03 by asaboure          #+#    #+#             */
+/*   Updated: 2021/12/29 17:01:33 by asaboure         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+#include <iostream>
+
+int main(void) 
+{
+	Fixed a;
+	Fixed b(a);
+	Fixed c;
+
+	c = b;
+	
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	return 0;
+}
