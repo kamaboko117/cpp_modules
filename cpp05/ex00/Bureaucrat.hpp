3bec250289447250162e6e3dbe57e8f0e622d057
Alexander SABOURET <asaboure@e2r11p14.clusters.42paris.fr>
2022-04-26 12:01:23 +0200

    catching up from rollback

--- cpp05/ex00/Bureaucrat.hpp
+++ cpp05/ex00/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: asaboure <asaboure@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/14 15:27:47 by asaboure          #+#    #+#             */
-/*   Updated: 2022/04/14 15:35:03 by asaboure         ###   ########.fr       */
+/*   Updated: 2022/04/26 12:00:46 by asaboure         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,21 @@ public:
 	~Bureaucrat();													//Canonical
 
 	Bureaucrat	&operator=(Bureaucrat const &rhs);					//Canonical
+
+	class GradeTooHighException : public std::exception
+	{
+	public:
+		virtual const char	*what() const throw(){
+			return ("Grade must be lower than 150");
+		}
+	};
+	class GradeTooLowException : public std::exception
+	{
+	public:
+		virtual const char	*what() const throw(){
+			return ("Grade must be higher than 0");
+		}
+	};
 
 	std::string	getName();
 	int			getGrade();
