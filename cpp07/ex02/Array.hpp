dd53b0126dd073391f59e57680a79dec99b9bb04
Alexander SABOURET <asaboure@e2r2p14.clusters.42paris.fr>
2022-05-10 20:13:35 +0200

    ex02 done

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: asaboure <asaboure@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/10 16:28:41 by asaboure          #+#    #+#             */
-/*   Updated: 2022/05/10 18:55:57 by asaboure         ###   ########.fr       */
+/*   Updated: 2022/05/10 20:07:58 by asaboure         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ template<typename T>
 class Array
 {
 private:
-	T 	*_tab;
-	int	_size;
+	T 				*_tab;
+	unsigned int	_size;
 public:
 	Array();															//CANON
 	Array(Array &src);													//CANON
@@ -27,7 +27,8 @@ public:
 	~Array();															//CANON
 
 	Array	&operator=(Array &rhs);										//CANON
-	T		&operator[](const int i);
+	T		&operator[](const unsigned int i);
+	int	size() const;
 };
 
 //CANON
@@ -45,7 +46,10 @@ Array<T>::Array(unsigned int n){
 
 template<typename T>
 Array<T>::Array(Array &src){
-	*this = src;
+	this->_size = src._size;
+	this->_tab = new T[this->_size];
+	for (unsigned int i = 0; i < this->_size; i++)
+		this->_tab[i] = src._tab[i];
 }
 
 template<typename T>
@@ -59,21 +63,28 @@ Array<T>	&Array<T>::operator=(Array &rhs){
 	if (this == &rhs)
 		return *this;
 	if (this->_size > 0)
-		delete [] this->_array;
+		delete [] this->_tab;
 	this->_size = rhs.size();
-	this->_array = new T[this->_size];
+	this->_tab = new T[this->_size];
 	for (int i = 0; i < this->_size; i++)
-		this->_array[i] = rhs[i];
+		this->_tab[i] = rhs[i];
 	return *this;
 }
 
 //OPERATORS
 
 template<typename T>
-T	&Array<T>::operator[](const int i){
+T	&Array<T>::operator[](const unsigned int i){
 	if (i >= this->_size || i < 0)
 		throw std::overflow_error("Index out of bounds");
-	return this->_array[i];
+	return this->_tab[i];
+}
+
+//ETC
+
+template<typename T>
+int	Array<T>::size() const{
+	return (this->_size);
 }
 
 #endif
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: asaboure <asaboure@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/10 18:12:29 by asaboure          #+#    #+#             */
-/*   Updated: 2022/05/10 18:48:10 by asaboure         ###   ########.fr       */
+/*   Updated: 2022/05/10 20:10:29 by asaboure         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,28 @@
 #include <iostream>
 
 int	main(){
-	Array<int> a(10);
+	Array<int> a(3);
+	
+	a[0] = 2;
+	a[1] = 42;
+	a[2] = 69;
+	std::cout << "a: ";
+	for (size_t i = 0; i < 3; i++)
+		std::cout << a[i] << ",";
+	std::cout << "size: " << a.size() << std::endl;
+	Array<int> b(a);
+	b[2] = 18;
+	std::cout << "a: ";
+	for (size_t i = 0; i < 3; i++)
+		std::cout << a[i] << ",";
+	std::cout << "size: " << a.size() << std::endl;
+	std::cout << "b: ";
+	for (size_t i = 0; i < 3; i++)
+		std::cout << b[i] << ",";
+	std::cout << "size: " << b.size() << std::endl;
+	try{
+		std::cout << a[-1] << std::endl;
+	}catch(const std::exception& e){
+		std::cout << e.what() << '\n';
+	}
 }
